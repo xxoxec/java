162957bf7086a9e79cc61a101061c5ec0f5cebdf
xxo <xxoxec@gmail.com>
2024-05-23 21:36:55 +0900

    반복문 ex

---
+++ src/Ioop/Ex/ForEx1.java
@@ -0,0 +1,11 @@
+package Ioop.Ex;
+
+public class ForEx1 {
+
+    public static void main(String[] args) {
+
+        for (int count = 1; count <= 10; count++) {
+            System.out.println(count);
+        }
+    }
+}
